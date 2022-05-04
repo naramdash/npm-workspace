07302812eb063167659538b08154171bbc1d8ea5
kimjuho_daim <kimjuho@daimresearch.com>
2022-05-04 17:11:53 +0900

    add package shared to package app

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,9 +1,10 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState } from "react";
+import logo from "./logo.svg";
+import { sayHello, sayGoodbye } from "shared";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
@@ -13,6 +14,12 @@ function App() {
         <p>
           <button type="button" onClick={() => setCount((count) => count + 1)}>
             count is: {count}
+          </button>
+          <button type="button" onClick={sayHello}>
+            hello to console
+          </button>
+          <button type="button" onClick={sayGoodbye}>
+            goodbye to console
           </button>
         </p>
         <p>
@@ -27,7 +34,7 @@ function App() {
           >
             Learn React
           </a>
-          {' | '}
+          {" | "}
           <a
             className="App-link"
             href="https://vitejs.dev/guide/features.html"
@@ -39,7 +46,7 @@ function App() {
         </p>
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
